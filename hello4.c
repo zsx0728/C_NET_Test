c2f081f283f0af153ef7f3a635685e81f1bec736
zsx0728 <zsx0728@163.com>
2021-01-13 17:41:08 +0800

    add hello*.c file

--- hello4.c
+++ hello4.c
@@ -9,5 +9,20 @@ main()
 {
 	int i;
 
-
+	initscr();
+	clear();
+	for (i=0;i<LINES;i++)
+	{
+		move(i, i+i);
+		if (i % 2 == 1)
+			standout();
+		addstr("Hello, world");
+		if (i % 2 == 1)
+			standend();
+		refresh();
+		sleep(1);
+		move(i, i+i);				/* move back */
+		addstr("            ");			/* erase line */
+	}
+	endwin();
 }
