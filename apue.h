9b51d072543961aee783d0dd61236088cbb61e78
zsx0728 <zsx0728@163.com>
2021-01-15 11:20:53 +0800

    20210115 add apue.h ...

---
+++ apue.h
@@ -0,0 +1,221 @@
+/* Our own header, to be included before all standard system headers */
+
+#ifndef _APUE_H
+#define _APUE_H
+
+#define _XOPEN_SOURCE 600				/* Single UNIX Specification, Version 3 */
+
+#include <sys/types.h>					/* some systems still require this */
+#include <sys/stat.h>
+#include <sys/termios.h>				/* for winsize */
+#ifndef TIOCGWINSZ
+#include <sys/ioctl.h>
+#endif
+#include <stdio.h>					/* for convenience */
+#include <stdlib.h>					/* for convenience */
+#include <stddef.h>					/* for offsetof */
+#include <string.h>					/* for convenience */
+#include <unistd.h>					/* for convenience */
+#include <signal.h>					/* for SIG_ERR */
+#define MAXLINE 4096					/* max line length */
+
+/*
+ * Default file access permissions for new files.
+ */
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+/*
+ * Default permissions for new directories.
+ */
+#define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
+typedef void Sigfunc(int);				/* for signal handlers */
+
+#if defined(SIG_IGN) && !defined(SIG_ERR)
+#define SIG_ERR((Sigfunc *)-1)
+#endif
+
+#define min(a,b)	((a) < (b) ? (a) : (b))
+#define max(a,b)	((a) > (b) ? (a) : (b))
+/*
+ * Prototypes for our own functions.
+ */
+char	*path_alloc(int *);
+long	open_max(void);
+void	clr_fl(int, int);
+void	set_fl(int, int);
+void	pr_exit(int);
+void	pr_mask(const char *);
+Sigfunc *signal_intr(int, Sigfunc *);
+
+int	tty_cbreak(int);
+int	tty_raw(int);
+int	tty_reset(int);
+void	tty_atexit(void);
+#ifdef ECHO /* only if <termios.h> has been included */
+struct termios *tty_termios(void);
+#endif
+
+void sleep_us(unsigned int);
+ssize_t readn(int, void *, size_t);
+ssize_t writen(int, const void *, size_t);
+void daemonize(const char *);
+
+int s_pipe(int *);
+int recv_fd(int, ssize_t (*func)(int,
+			const void *, size_t));
+int send_fd(int, int);
+int send_err(int, int,
+		const char *);
+int serv_listen(const char *);
+int serv_accept(int, uid_t *);
+int cli_conn(const char *);
+int buf_args(char *, int (*func)(int,
+			char **));
+int ptym_open(char *, int);
+int ptys_open(char *);
+#ifdef TIOCGWINSZ
+pid_t pty_fork(int *, char *, int, const struct termios *,
+		const struct winsize *);
+#endif
+int lock_reg(int, int, int, off_t, int, off_t);
+#define read_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+#define readw_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLKw, F_RDLCK, (offset), (whence), (len))
+#define write_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
+#define writew_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+#define un_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
+pid_t lock_test(int, int, off_t, int, off_t);
+#define is_read_lockable(fd, offset, whence, len) \
+	(lock_test((fd), F_RDLCK, (offset), (whence), (len)) == 0)
+#define is_write_lockable(fd, offset, whence, len) \
+	(lock_test((fd), F_WRLCK, (offset), (whence), (len)) == 0)
+
+void err_dump(const char *, ...);
+void err_msg(const char *, ...);
+void err_quit(const char *, ...);
+void err_exit(int, const char *, ...);
+void err_ret(const char *, ...);
+void err_sys(const char *, ...);
+
+void log_msg(const char *, ...);
+void log_open(const char *, int, int);
+void log_quit(const char *, ...);
+void log_ret(const char *, ...);
+void log_sys(const char *, ...);
+
+void TELL_WAIT(void);
+void TELL_PARENT(pid_t);
+void TELL_CHILD(pid_t);
+void WAIT_PARENT(void);
+void WAIT_CHILD(void);
+
+#endif /* _APUE_H */
+
+//程序清单B-2
+#include <errno.h>				/* for definition of errno */
+#include <stdarg.h>				/* ISO C varibale aruments */
+
+static void err_doit(int, int, const char *, va_list);
+
+/*
+ * Nonfatal error related to a system call.
+ * Print a message and return.
+ */
+void err_ret(const char *fmt, ...)
+{
+	va_list	ap;
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+}
+
+/*
+ * Fatal error related to a system call.
+ * Print a message and terminate.
+ */
+void err_sys(const char *fmt, ...)
+{
+	va_list	ap;
+
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/*
+ * Fatal error unrelated to a system call.
+ * Error code passed as explict parameter.
+ * Print a message and terminate.
+ */
+void err_exit(int error, const char *fmt, ...)
+{
+	va_list	ap;
+
+	va_start(ap, fmt);
+	err_doit(1, error, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/*
+ * Fatal error related to a system call.
+ * Print a message, dump core, and terminate.
+ */
+void err_dump(const char *fmt, ...)
+{
+	va_list	ap;
+
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	abort();				/* dump core and terminate */
+	exit(1);				/* shouldn't get here */
+}
+
+/*
+ * Nonfatal error unrelated to a system call.
+ * Print a message and return.
+ */
+void err_msg(const char *fmt, ...)
+{
+	va_list	ap;
+
+	va_start(ap, fmt);
+	err_doit(0, 0, fmt, ap);
+	va_end(ap);
+}
+
+/*
+ * Fatal error unrelated to a system call.
+ * Print a message and terminate.
+ */
+void err_quit(const char *fmt, ...)
+{
+	va_list	ap;
+
+	va_start(ap, fmt);
+	err_doit(0, 0, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/*
+ * Print a message and return to caller.
+ * Caller specifies "errnoflag".
+ */
+static void err_doit(int errnoflag, int error, const char * fmt, va_list ap)
+{
+	char	buf[MAXLINE];
+	vsnprintf(buf, MAXLINE, fmt, ap);
+	if (errnoflag)
+		snprintf(buf+strlen(buf), MAXLINE-strlen(buf), ": %s",
+				strerror(error));
+	strcat(buf, "\n");
+	fflush(stdout);				/* in case stdout and stderr are the same */
+	fputs(buf, stderr);
+	fflush(NULL);				/* flushes all stdio output streams */
+}
