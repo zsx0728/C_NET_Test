e0d594e1f07cbbbf491e68eea32214ac3d567a89
zsx0728 <zsx0728@163.com>
2021-01-20 17:44:13 +0800

    add programlist4_9.c

---
+++ programlist4_9.c
@@ -0,0 +1,17 @@
+#include        "programlist2_3.c"
+
+int
+main(void)
+{
+	char	*ptr;
+	int	size;
+	if (chdir("/usr/spool/uucppublic") < 0)
+		err_sys("chdir failed");
+
+	ptr = path_alloc(&size);				/* our own function */
+	if (getcwd(ptr, size) == NULL)
+		err_sys("getcwd failed");
+
+	printf("cwd = %s\n", ptr);
+	exit(0);
+}
