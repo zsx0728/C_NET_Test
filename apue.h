0e2f89028311e0f6b5ac89f2d5d54cbfcfe90fb9
zsx0728 <zsx0728@163.com>
2021-01-23 16:17:26 +0800

    add programlist8_17.c

--- apue.h
+++ apue.h
@@ -240,3 +240,37 @@ pr_exit(int status)
 		printf("child stooped, signal number = %d\n",
 				WSTOPSIG(status));
 }
+
+int
+system(const char *cmdstring)				/* version without signal handling */
+{
+	pid_t	pid;
+	int	status;
+
+	if (cmdstring == NULL)
+		return(1);				/* always a command processor with UNIX */
+
+	if ((pid = fork()) < 0)
+	{
+		status = -1;				/* probably out of processes */
+	}
+	else if (pid == 0)
+	{
+		/* child */
+		execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
+		_exit(127);				/* execl error */
+	}
+	else
+	{
+		while (waitpid(pid, &status, 0) < 0)
+		{
+			if (errno != EINTR)
+			{
+				status = -1;		/* error other than EINTR from waitpid() */
+				break;
+			}
+		}
+	}
+
+	return(status);
+}
