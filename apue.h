cc17bc3da7a4855946af089431e682cbf17c3d2c
zsx0728 <zsx0728@163.com>
2021-02-01 13:59:21 +0800

    add programlist12_2.c

--- apue.h
+++ apue.h
@@ -297,3 +297,22 @@ pr_mask(const	char *str)
 	printf("\n");
 	errno = errno_save;
 }
+
+#include	<pthread.h>
+
+int
+makethread(void	*(*fn)(void	*), void	*arg)
+{
+	int		err;
+	pthread_t	tid;
+	pthread_attr_t	attr;
+
+	err = pthread_attr_init(&attr);
+	if (err != 0)
+		return(err);
+	err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	if (err == 0)
+		err = pthread_create(&tid, &attr, fn, arg);
+	pthread_attr_destroy(&attr);
+	return(err);
+}
