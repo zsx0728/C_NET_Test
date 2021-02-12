25e41a877ba28b510d08aeb3f62bf222c9ed760e
zsx0728 <zsx0728@163.com>
2021-02-12 21:09:23 +0800

    add changeenv.c

---
+++ showenv.c
@@ -0,0 +1,13 @@
+/* showenv.c - shows how to read and print the environment
+ */
+#include	<stdio.h>
+
+extern	char	**environ;						/* points to the array of strings */
+
+main()
+{
+	int	i;
+
+	for(i = 0; environ[i]; i++ )
+		printf("%s\n", environ[i]);
+}
