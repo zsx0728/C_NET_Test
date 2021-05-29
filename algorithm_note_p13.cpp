164923041a712ecee258199a2461d60d233b42fc
zsx0728 <zsx0728@163.com>
2021-05-29 08:39:40 +0800

    add algorithm_note_p13.cpp

---
+++ algorithm_note_p13.cpp
@@ -0,0 +1,27 @@
+#include	<stdio.h>
+int main(void)
+{
+	int a[110];
+	int n, m, count = 0;
+	scanf("%d%d", &n, &m);
+	m = m % n;
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	
+	for (int i = n - m; i < n; i++)
+	{
+		printf("%d", a[i]);
+		count++;
+		if (count < n) printf(" ");
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		count++;
+		if (count < n) printf(" ");
+	}
+	return 0;
+}
