8beaf5540d680ec052a489ac60215984dd3892d8
zsx0728 <zsx0728@163.com>
2021-06-24 08:49:39 +0800

    add algorithm_note_p126.cpp

---
+++ algorithm_note_p126.cpp
@@ -0,0 +1,28 @@
+#include	<stdio.h>
+
+int binarySearch(int A[], int left, int right, int x)
+{
+	int mid;
+	while(left <= right)
+	{
+		mid = (left + right) / 2;
+		if (A[mid] == x) return mid;
+		else if (A[mid] > x)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	return -1;
+}
+
+int main(void)
+{
+	const int n = 10;
+	int A[n] = {1, 3, 4, 6, 7, 8, 10, 11, 12, 15};
+	printf("%d %d\n", binarySearch(A, 0, n-1, 6), binarySearch(A, 0, n-1, 9));
+	return 0;
+}
