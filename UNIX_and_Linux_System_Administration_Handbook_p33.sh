61dcd14cd508efa33e3de7373892f211c56ca1e6
zsx0728 <zsx0728@163.com>
2021-06-25 10:57:45 +0800

    add UNIX_and_Linux_System_Administration_Handbook_p33.sh

---
+++ UNIX_and_Linux_System_Administration_Handbook_p33.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -n "Enter your name:"
+read user_name
+
+if [ -n "$user_name" ];then
+	echo "Hello $user_name!"
+	exit 0
+else
+	echo "You did not tell me your name!"
+	exit 1
+fi
