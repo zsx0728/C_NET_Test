2360e7e9811b0f08c5cf38fbaf9b239eb43a0a51
zsx0728 <zsx0728@163.com>
2021-06-28 15:50:27 +0800

    add UNIX_and_Linux_System_Administration_Handbook_p39.sh

---
+++ UNIX_and_Linux_System_Administration_Handbook_p39.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+example=(aa 'bb cc' dd)
+example[3]=ee
+
+echo "example[@] = ${example[@]}"
+echo "example array contains ${#example[@]} elements"
+
+for elt in "${example[@]}"; do
+	echo " Element = $elt"
+done
