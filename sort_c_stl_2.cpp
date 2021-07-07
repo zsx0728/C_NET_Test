669f87416be67546530f223f0b095a8be8c30f0d
zsx0728 <zsx0728@163.com>
2021-07-07 15:25:37 +0800

    add sort_c_stl_2.cpp

--- sort_c_stl_2.cpp
+++ sort_c_stl_2.cpp
@@ -6,12 +6,10 @@ int main(void)
 	int arr[] = {1,5,8,9,6,7,3,4,2,0};
 	int n = sizeof(arr) / sizeof(arr[0]);
 
-	sort(arr, arr + n);
+	sort(arr, arr + n, greater<int>());
 
-	cout << "\nArray after sorting using "
-		"default sort is : \n";
+	cout << "Array after sorting : \n";
 	for (int i = 0; i < n; ++i)
 		cout << arr[i] << " ";
-
 	return 0;
 }
